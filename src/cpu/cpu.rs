c8ef332d1ed419472986f9f3bfcdb02a8f26addf
Borja Lorente <blorente@ucm.es>
2017-08-19 17:04:12 +0200

    Take special regs out of the regbank

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -34,19 +34,15 @@ pub struct RegBank {
     bc : Register,
     de : Register,
     hl : Register,
-    sp : Register,
-    pc : Register,
 }
 
 impl fmt::Display for RegBank {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(fmt, "a: 0x{:<2x} | f: 0x{:<2x}\nb: 0x{:<2x} | c: 0x{:<2x}\nd: 0x{:<2x} | e: 0x{:<2x}\nh: 0x{:<2x} | l: 0x{:<2x}\nsp: 0x{:<4x}\npc: 0x{:<4x}",
+        writeln!(fmt, "a: 0x{:<2X} | f: 0x{:<2X}\nb: 0x{:<2X} | c: 0x{:<2X}\nd: 0x{:<2X} | e: 0x{:<2X}\nh: 0x{:<2X} | l: 0x{:<2X}",
         self.af.r_hi(), self.af.r_lo(),
         self.bc.r_hi(), self.bc.r_lo(),
         self.de.r_hi(), self.de.r_lo(),
-        self.hl.r_hi(), self.hl.r_lo(),
-        self.sp.value(),
-        self.pc.value())
+        self.hl.r_hi(), self.hl.r_lo())
     }
 }
 
@@ -57,8 +53,6 @@ impl Default for RegBank {
             bc: Register {val: 0x0000},
             de: Register {val: 0x0000},
             hl: Register {val: 0x0000},
-            sp: Register {val: 0x0000},
-            pc: Register {val: 0x0000},
         }
     }
 }
@@ -82,6 +76,8 @@ impl<'i> Instruction<'i> {
 
 pub struct CPU <'i> {
     registers : RegBank,
+    sp : Register,
+    pc : Register,
     instructions : [Instruction<'i>; 256]
 }
 
@@ -91,20 +87,25 @@ impl<'i>  Default for CPU<'i> {
         CPU {
             registers : Default::default(),
             instructions : create_isa(),
+            sp : Default::default(),            
+            pc : Default::default(),
         }
     }
 }
 
 impl<'i> fmt::Display for CPU<'i> {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(fmt, "{}", self.registers)
+        writeln!(fmt, "{}sp: 0x{:<4X}\npc: 0x{:<4X}", 
+                self.registers,
+                self.sp.value(),
+                self.pc.value())
     }
 }
 
 impl<'i> CPU<'i> {
     pub fn run(&mut self, rom_buf: Box<[u8]>) {
         loop {
-            let opcode = rom_buf[self.registers.pc.value() as usize];
+            let opcode = rom_buf[self.pc.value() as usize];
             println!("Running inst {:X}", opcode);
             if self.instructions[opcode as usize].dissassembly == "Unimp" {
                 println!("Unimplemented instruction 0x{:X}", opcode);
