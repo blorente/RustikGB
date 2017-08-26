58b1c7242bcef2097d835022d16590bc71adc96e
Borja Lorente <blorente@ucm.es>
2017-08-26 19:16:15 +0200

    Add printing a memory location

--- src/hardware/debugger.rs
+++ src/hardware/debugger.rs
@@ -34,16 +34,27 @@ impl Debugger {
                 cpu.read_byte(pc));
         println!("Processor state:\n{}", cpu);
 
-        let mut good_command = false;
+        let mut stop_asking = false;
         println!("Debug Command: ");
-        let mut command: String = read!();
-        while !good_command {
-            match &*command {
-                "n" => {self.state = DebuggerState::STEP; good_command = true}
-                "c" => {self.state = DebuggerState::RUN; good_command = true}                
-                _ => {print!("Debug Command: "); command = read!();}
+        let mut command: String = read!("{}\n");
+        while !stop_asking {            
+            match &command[..1] {
+                "n" => {self.state = DebuggerState::STEP; stop_asking = true}
+                "c" => {self.state = DebuggerState::RUN; stop_asking = true}   
+                "p" => {
+                    let addr : u16 = u16::from_str_radix(&command[4..8], 16).unwrap();
+                    self.print_addr(addr, cpu);                    
+                    }             
+                _ => {}
             }
+
+            if !stop_asking {print!("Debug Command: "); command = read!("{}\n");}
         }
+    }
+
+    fn print_addr(&self, addr: u16, cpu: &CPU) {
+        let val = cpu.read_byte(addr);
+        println!("Addr {:04X} contains {:02X}", addr, val);
     }
 }
 
@@ -61,5 +72,6 @@ macro_rules! hash {
 
 fn create_breakpoints() -> HashSet<u16> {
     hash![
+        //0x66
     ]
 }
