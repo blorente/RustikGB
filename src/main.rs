91114d897427799d6108b9fe2d1dadc8189ecd6f
Borja Lorente <blorente@ucm.es>
2017-08-27 14:10:27 +0200

    Some resemblance of a screen

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+
 mod hardware;
+use hardware::video::screen::SCREEN_DIMS;
 
 use piston_window::*;
-use texture::*;
 
 #[macro_use] extern crate text_io;
 extern crate piston_window;
-
-const SCREEN_DIMS: [u32; 2] = [160, 144];
+extern crate rand;
 
 fn main() {
     let boot_buf = read_bin("assets/BIOS.gb");
@@ -21,11 +21,19 @@ fn main() {
 
     let bus = hardware::memory::bus::BUS::new(boot_buf, cartridge);
     let mut processor : hardware::cpu::CPU = hardware::cpu::CPU::new(bus);
+    let mut screen = hardware::video::screen::Screen::new(&mut window);
 
     while let Some(e) = window.next() {
-        window.draw_2d(&e, |c, g| {
-            clear([0.5, 1.0, 0.5, 1.0], g);
-        });
+        match e {
+            Event::Render(_) => {
+                screen.update(&mut window, e);
+
+                screen.set_pixel(rand::random::<u8>() % 160, rand::random::<u8>() % 140, 224, 51, 224);
+            }
+            _ => {}
+        }
+
+        // Here we CPU.run_frame() or something
     }
     processor.run();
 }
@@ -40,10 +48,13 @@ fn read_bin(path: &'static str) -> Box<[u8]> {
 }
 
 fn init_window() -> PistonWindow {
-    let mut window: PistonWindow = WindowSettings::new("RustikGB", (SCREEN_DIMS[0] * 2, SCREEN_DIMS[1] * 2))
+    let mut window: PistonWindow = WindowSettings::new("RustikGB", 
+        (hardware::video::screen::SCREEN_DIMS[0] * 2, 
+         hardware::video::screen::SCREEN_DIMS[1] * 2))
         .resizable(false)
         .exit_on_esc(true)
         .build()
         .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+    window.set_ups(60);
     window
 }
